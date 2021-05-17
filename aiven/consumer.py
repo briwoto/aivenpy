1c6a0ff6cbfe2f0bd2d0d57670fc8e0e326fff02
Rahul Singh <rahul.singh@sennder.com>
2021-05-18 00:43:34 +0200

    explicit exception handling for kafka consumer

--- aiven/consumer.py
+++ aiven/consumer.py
@@ -2,19 +2,22 @@ from apps.consumer import consumer_queries
 from apps.consumer import kf_consumer
 import config
 import atexit
-log = config.get_logger()
 config.create_pem_file()
 
 
 def start_consumer():
+    msg_object = kf_consumer.get_consumer()
+    if isinstance(msg_object, Exception):
+        log.error(f'Kafka Consumer CONNECTION EXCEPTION:\n{type(msg_object).__name__}: {str(msg_object)}')
+        quit()
     try:
-        msg_object = kf_consumer.get_consumer()
         for message in msg_object:
             consumer_queries.add_site_monitor_data_to_db((message.key, message.value))
     except Exception as e:
-        log.error(f'Kafka consumer EXCEPTION OCCURED:\n{str(e)}')
+        log.error(f'Kafka messages EXCEPTION OCCURED:\n{str(e)}')
 
 
 if __name__ == '__main__':
+    log = config.get_logger()
     atexit.register(config.delete_pem_at_exit)
     start_consumer()
--- apps/consumer/kf_consumer.py
+++ apps/consumer/kf_consumer.py
@@ -4,16 +4,19 @@ from apps.common import serializer
 
 
 def get_consumer():
-    return KafkaConsumer(
-        'site-monitor',
-        bootstrap_servers=os.environ.get("BOOTSTRAP_SERVER"),
-        sasl_plain_username=os.environ.get("KF_USER"),
-        sasl_plain_password=os.environ.get("KF_PASSWORD"),
-        security_protocol="SASL_SSL",
-        ssl_cafile="apps/common/ca.pem",
-        sasl_mechanism="PLAIN",
-        group_id='monitor-group',
-        auto_offset_reset='earliest',
-        value_deserializer=lambda v: serializer.json_deserialize(v),
-        key_deserializer=lambda k: serializer.json_deserialize(k),
-    )
+    try:
+        return KafkaConsumer(
+            'site-monitor',
+            bootstrap_servers=os.environ.get("BOOTSTRAP_SERVER"),
+            sasl_plain_username=os.environ.get("KF_USER"),
+            sasl_plain_password=os.environ.get("KF_PASSWORD"),
+            security_protocol="SASL_SSL",
+            ssl_cafile="apps/common/ca.pem",
+            sasl_mechanism="PLAIN",
+            group_id='monitor-group',
+            auto_offset_reset='earliest',
+            value_deserializer=lambda v: serializer.json_deserialize(v),
+            key_deserializer=lambda k: serializer.json_deserialize(k),
+        )
+    except Exception as e:
+        return e
